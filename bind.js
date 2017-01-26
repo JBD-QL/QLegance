979a542fcb2d804b0a253673175b377664295196
Dhani Mayfield <mayfield_dhani@dhanimay.com>
2017-01-26 15:18:38 -0800

    Update bind.js

--- bind.js
+++ bind.js
@@ -1,7 +1,6 @@
 'use strict';
 
-let QLegance = (()=>{
-<<<<<<< HEAD
+let QL = (()=>{
 
   return new Binder();
 
@@ -34,6 +33,7 @@ let QLegance = (()=>{
 
         type = getAttr(component.element).split('|');
 
+        component.partial = component.element.cloneNode(true);
         component.type_name = type[0].trim(); // ie. a schema defined for User
         component.staged_query = type[1].trim(); // ie. getUser(limit: 1)
         component.fields = [];
@@ -86,14 +86,18 @@ let QLegance = (()=>{
             QLegance[method](args, returnValues).then((result) => {
               let component = Client.components.find( component => { return selection === component.element; });
               //if(!component) component = buildcomponent(selection);
-              populate(component, result.data);
+              prePopulate(component, result.data);
               resolve(result.data);
             });
           })
         };
 
-        wrapper.query = (fragment) => {
-
+        wrapper.query = (method, args, returnValues) => {
+          return single[method](args, returnValues).then((result) => {
+            let component = Client.components.find( component => { return selection === component.element; });
+            prePopulate(component, result.data);
+            return result.data;
+          });
         };
         return wrapper;
       }
@@ -103,34 +107,24 @@ let QLegance = (()=>{
       }
 
       function buildQuery(component){
+        let str = component.staged_query;
+        if(str[str.length - 1] === ')' && str[str.length - 2] === '('){
+          str = parseStagedQuery(str);
+        }
+
         return `{
-          ${component.staged_query}{
+          ${str}{
             ${component.fields.map(field => {return field.name; }).join('\n')}
           }
         }`;
       }
 
       function sendQuery(query){
-=======
-  class Binder{
-    constructor(){
-      let server = '';
-
-      this.setServer = (serv) => { 
-        server = serv;
-        this.introspect();
-      };
-
-      this.getServer = () => { return server; };
-
-      this.sendQuery = (query) => {
->>>>>>> c7fcd3e7c3b1061796b47a250bdcf09c8f7bc455
         return new Promise((resolve, reject) => {
           let xhr = new XMLHttpRequest();
 
           xhr.open("POST", server, true);
           xhr.setRequestHeader("Content-Type", "application/json");
-<<<<<<< HEAD
         	xhr.send(JSON.stringify({query}));
 
           xhr.onreadystatechange = () => {
@@ -173,57 +167,34 @@ let QLegance = (()=>{
 
       function prePopulate(component, data){
         let input,
-         template, len, element, html = '';
-        let queryKey = parseStagedQuery(component.staged_query);
+         template, len, element, html = '', value;
+
+        //let queryKey = parseStagedQuery(component.staged_query);
+        let queryKey = Object.keys(data)[0];
 
         data = data[queryKey];
-        len =  component.list ? data.length : 1;
-        data = len > 1 ? data : [data];
+        len = data.length || 1;
+        data = Array.isArray(data) ? data : [data];
 
         for(let i = 0; i < len; i++){
-          template = component.element.cloneNode(true);
-
+          template = component.partial.cloneNode(true);
           component.fields.forEach((field) => {
             element = template.querySelector(`[ql-field=${field.name}]`);
             if(element.nodeName.toLowerCase() === 'input'){
+              if(data[i][field.name] === 'undefined') return;
               element.setAttribute('value', data[i][field.name]);
             }else{
+              if(data[i][field.name] === 'undefined') return;
               element.innerHTML = data[i][field.name];
             }
           });
           html += template.innerHTML;
         }
+
         component.element.innerHTML = html;
       }
 
     function introspect() {
-=======
-          xhr.send(JSON.stringify({query: query}));
-
-          xhr.onreadystatechange = () => {
-            if(xhr.status === 200 && xhr.readyState === 4){
-              let response = JSON.parse(xhr.response);
-              // this.cacheQuery(query, response.data);
-              resolve(response);
-            }else if(xhr.status > 400 && xhr.status < 500){
-              reject(xhr.status)
-            }
-          };
-        });
-      };
-
-      this.query = (string) => {
-        return this.sendQuery("query" + string);
-      };
-
-      this.mutate = (string) => {
-        return this.sendQuery("mutation" + string);
-      };
-    }
-    // end of constructor
-
-    introspect() {
->>>>>>> c7fcd3e7c3b1061796b47a250bdcf09c8f7bc455
       const introspectiveQuery = `
         {
           __schema {
@@ -275,11 +246,7 @@ let QLegance = (()=>{
         }
       `;
 
-<<<<<<< HEAD
       sendQuery(introspectiveQuery)
-=======
-      this.sendQuery(introspectiveQuery)
->>>>>>> c7fcd3e7c3b1061796b47a250bdcf09c8f7bc455
         .then((res) => {
           // console.log(res);
           const fields = [];
@@ -294,8 +261,7 @@ let QLegance = (()=>{
             fields.push(res.data.__schema.queryType.fields[i]);
           }
           // res.data.__schema.mutationType.fields.concat(res.data.__schema.queryType.fields);
-          console.log('fields:', fields);
-<<<<<<< HEAD
+          //console.log('fields:', fields);
           //this.types = {};
           // loop through fields
           for (let i = 0; i < fields.length; i += 1) {
@@ -403,96 +369,3 @@ let QLegance = (()=>{
   //     }
   //     return JSON.stringify('QLegance:' + hash);
   //   }
-=======
-          this.types = {};
-          // loop through fields
-          for (let i = 0; i < fields.length; i += 1) {
-            this.typeFieldConstructor(fields[i]);
-            this.methodConstructor(fields[i]);
-          }
-          console.log(this);
-        });
-    }
-
-    typeFieldConstructor(field) {
-      if (!this.types[field.type.name]) {
-        if (field.type.name) {
-          this.types[field.type.name] = [];
-        } else {
-          this.types[field.type.kind] = [];
-        }
-      }
-      if (field.type.name) {
-        // console.log('valid type');
-        this.types[field.type.name].push(field.name);
-      } else {
-        this.types[field.type.kind].push(field.name);
-      }
-    }
-
-    methodConstructor(field) {
-      // ex: QLegance.field_name({}, [returning values])
-
-      // construct tempFunc based on information in field
-      const tempFunc = (obj, arr) => {
-        let returnValues = arr.join('\n');
-
-        // field that does take arguments
-        if (field.args.length) {
-          let args = '';
-          for (let i = 0 ; i < field.args.length; i += 1) {
-            let end = ''
-            if (i < field.args.length - 1) end += ', ';
-
-            if (field.args[i].type.kind === 'NON_NULL') {
-              if (field.args[i].name in obj) {
-                const item = this.typeConverter(field.args[i].type.ofType.name, obj[field.args[i].name]);
-                args += `${field.args[i].name} : ${item}${end}`
-              }
-            } else {
-              if (field.args[i].name in obj) {
-                const item = this.typeConverter(field.args[i].type.name, obj[field.args[i].name]);
-                args += `${field.args[i].name} : ${item}${end}`
-              }
-            }
-          }
-          return this.sendQuery(`
-            ${field.query} {
-              ${field.name}(${args}) {
-                ${returnValues}
-              }
-            }
-          `);
-
-        // field that does not take arguments
-        } else {
-          return this.sendQuery(`
-            ${field.query} {
-              ${field.name} {
-                ${returnValues}
-              }
-            }
-          `);
-        }
-      }
-
-      // append tempFunc as method to QLegance object with the associted field name
-      this[field.name] = tempFunc;
-    }
-
-    typeConverter(type, item) {
-      if (type === 'String') {
-        return `"${item}"`;
-      }
-      if (type === 'Int' || type === 'Float') {
-        return Number(item);
-      }
-    }
-  }
-  // end of bind class
-
-  const binder = new Binder();
-  console.log('binder:', binder);
-  return binder;
-})();
->>>>>>> c7fcd3e7c3b1061796b47a250bdcf09c8f7bc455
